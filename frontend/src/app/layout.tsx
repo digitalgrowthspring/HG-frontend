76d1a3711086be8982dc7f8fcd4ab0eaa78c8a86
Ryan <ryan@digitalgrowthspring.com>
2026-04-03 21:24:57 +0200

    Perf: migrate fonts to next/font and optimize GTM payload injection to resolve CLS

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,22 @@
 import type { Metadata } from "next";
 import Script from "next/script";
+import { Fredoka, Nunito } from "next/font/google";
 import "./globals.css";
 import { Header, Footer } from "@/components/Navigation";
 import { HeroMagic } from "@/components/HeroMagic";
 import { SITE_NAME, SITE_URL } from "@/lib/seo";
+
+const fredoka = Fredoka({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-fredoka",
+});
+
+const nunito = Nunito({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-nunito",
+});
 
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
 
@@ -29,32 +42,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${fredoka.variable} ${nunito.variable}`}>
       <head>
+      </head>
+      <body style={{ margin: 0 }}>
         {gtmId ? (
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <>
+            <Script
+              id="gtm-script"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${gtmId}');`,
-            }}
-          />
-        ) : null}
-      </head>
-      <body style={{ margin: 0 }}>
-        {gtmId ? (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
+              }}
             />
-          </noscript>
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          </>
         ) : null}
         <HeroMagic />
         <Header />
